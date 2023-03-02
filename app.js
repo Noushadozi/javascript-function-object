b0c05ab7679aeee296f97153c81d0e56ade140cd
Noushadozi <noushadozi333@gmail.com>
2023-03-02 22:35:48 +0600

    done

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
-// for(var i = 0; i < 20; i++){
-//     console.log('rabbana hablana min ajwajina wa jurriya tina kurrata aiwun');
-// }
+for(var i = 0; i < 20; i++){
+    console.log('rabbana hablana min ajwajina wa jurriya tina kurrata aiwun');
+}
 
-// const friends = ["loneliness", "awkwardness", "nervousness", "depression"];
+const friends = ["loneliness", "awkwardness", "nervousness", "depression"];
 
-// for(var i = 0; i < friends.length; i++){
-//     var element = friends[i];
-//     console.log(element);
-// }
-// var human = {
-//     name: "Noushad",
-//     age: 23,
-//     friends: ["loneliness", "awkwardness", "nervousness", "depression"],
-//     country: "Bangladesh",
-//     girlfriend: "Protibondir girlfriend hoa na",
-//     wife: {
-//         name: "really??",
-//         age: "LOL",
-//         edu: "tor baper matha"
-//     }
-// };
+for(var i = 0; i < friends.length; i++){
+    var element = friends[i];
+    console.log(element);
+}
+var human = {
+    name: "Noushad",
+    age: 23,
+    friends: ["loneliness", "awkwardness", "nervousness", "depression"],
+    country: "Bangladesh",
+    girlfriend: "Protibondir girlfriend hoa na",
+    wife: {
+        name: "really??",
+        age: "LOL",
+        edu: "tor baper matha"
+    }
+};
 
-// console.log(human.friends[3]);
+console.log(human.friends[3]);
 
-// for(let i = 0; i < human.friends.length; i++){
-//     var element = human.friends[i];
-//     console.log(element);
-// }
+for(let i = 0; i < human.friends.length; i++){
+    var element = human.friends[i];
+    console.log(element);
+}
 
-// function rahim(){
-//     console.log("hoo bhai");
-// }
+function rahim(){
+    console.log("hoo bhai");
+}
 
-// rahim();
+rahim();
 
-// function sum(num1, num2){
-//     var result = (num1 + num2);
-//     return result;
-// }
-// var totalSum = sum(10, 20);
+function sum(num1, num2){
+    var result = (num1 + num2);
+    return result;
+}
+var totalSum = sum(10, 20);
 
-// function minus(num1, num2){
-//     var result = (num1 - num2);
-//     return result;
-// }
-// var totalMinus = minus(30, 20);
+function minus(num1, num2){
+    var result = (num1 - num2);
+    return result;
+}
+var totalMinus = minus(30, 20);
 
-// var answer = totalSum * totalMinus;
-// console.log(answer);
+var answer = totalSum * totalMinus;
+console.log(answer);
 
-// function kmToMiles(km) {
-//   const result = km * 0.62;
-//   return result;
-// }
-// const miles = kmToMiles(12);
-// console.log(parseFloat(miles.toFixed(2)));
+function kmToMiles(km) {
+  const result = km * 0.62;
+  return result;
+}
+const miles = kmToMiles(12);
+console.log(parseFloat(miles.toFixed(2)));
 
 
 
@@ -68,9 +68,38 @@ function workout(workoutName){
         return bicepWorkout;
     }
     else{
-        console.log("Fitness doesn't matter mentality does");
+        return "Fitness doesn't matter mentality does";
     }
 }
 
 var workoutPlan = workout("-_-");
 console.log(workoutPlan);
+
+
+
+
+var firstToTenDays = 500;
+var second11To20Days = 300;
+var third21to30days = 100;
+
+function cost(myDays){
+    if(myDays <= 10){
+        var totalCost = myDays * 500;
+        return totalCost;
+    }
+    else if(myDays <= 20 && myDays >= 10){
+        var first10 = 500 * 10; 
+        var remaining = 300 * (myDays - 10);
+        var total = first10 + remaining;
+        return total;
+    }
+    else{
+        var first10 = 500 * 10; 
+        var second10 = 300 * 10; 
+        var remaining = 300 * (myDays - 20);
+        var total = first10 + second10 + remaining;
+        return total;
+    }
+}
+const totalCost = cost(29);
+console.log(totalCost);
